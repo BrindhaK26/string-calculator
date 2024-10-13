c655ff67f2d55265190cd74346328e21fa9a5815
brindha-k <brindha.k@samsung.com>
2024-10-13 14:00:02 +0530

    Logic to handle differernt delimiters of varying lengths

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const prompt = require('prompt-sync')();
 
 const stringCalc = new StringCalculator();
 const userInput = prompt("Please enter string of Integers:");
-console.log(typeof userInput);
 try{
   console.log("Sum of all the integers in the given string is : ", stringCalc.add(userInput))
 }catch(e){
