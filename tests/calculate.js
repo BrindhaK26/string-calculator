a0657b8c2bdf6f815975c3e9e152182f4231b72f
brindha-k <brindha.k@samsung.com>
2024-10-13 13:31:13 +0530

    Include function call count

--- tests/calculate.js
+++ tests/calculate.js
@@ -1,17 +1,32 @@
 const StringCalculator =  require("../lib/calculate");
 const expect = require("chai").expect;
 
-describe('Testing the Add Functions', function() {
+describe("Testing the Add Functions", function() {
     let stringCalculator;
-    before(()=>{
+    beforeEach(()=>{
         stringCalculator = new StringCalculator();
     })
-    it("1. Add accepts n no.of inputs serperated by ',' as delimiter", function(done) {
+    it("should accept single input", (done) => {
+        expect(stringCalculator.add("1")).to.equal(1);
+        done();
+    });
+    
+    it("should accept 2 inputs serperated by ',' as delimiter", (done) => {
+        expect(stringCalculator.add("1,2")).to.equal(3);
+        done();
+    });
+    it("should accept n no.of inputs serperated by ',' as delimiter", function(done) {
         expect(stringCalculator.add("1,2,6,3,6,,7")).to.equal(25);
         done();
     });
-    it("1. return 0 when empty string is passed", function(done) {
+    it("return 0 when empty string is passed", function(done) {
         expect(stringCalculator.add("")).to.equal(0);
         done();
     });
+    it("should count how many times Add() was called", (done) => {
+        stringCalculator.add("1,2");
+        stringCalculator.add("3,4");
+        expect(stringCalculator.GetCalledCount()).to.equal(2);
+        done();
+      });
 })
